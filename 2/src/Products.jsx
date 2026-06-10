3ea1dbd560965c2983ab1f8879a8eed6e7d190a6
Lucky Sah <lk4179869@gmail.com>
2026-06-10 18:27:51 +0530

    add: continal statements

--- 2/src/Products.jsx
+++ 2/src/Products.jsx
@@ -1,10 +1,12 @@
 import "./Products.css"
 export default function Products(props){
+    let isDiscount= props.price>10000 ? "50% off" : null
+    let styles= {backgroundColor:"yellow"}
     return(
         <div className= "container">
         <p>{props.name}</p>
         <p>{props.description}</p>
-        <p>{props.price}</p>
+        <h1 style={styles}>{isDiscount}</h1>
         <p>{props.features.join(", ")}</p>
         </div>
     )
