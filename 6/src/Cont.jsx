2694f7e9d98f35bb7030233716a08a1b689e12a5
Lucky Sah <lk4179869@gmail.com>
2026-06-10 19:02:32 +0530

    add: callback in state

--- 6/src/Cont.jsx
+++ 6/src/Cont.jsx
@@ -2,7 +2,15 @@ import { useState } from "react"
 export default function Cont(){
     let[count, setCount]= useState(0);
     function incCount(){
-        setCount(count+1)
+        setCount((currValue)=>{
+            console.log(currValue);
+            return currValue+1
+        });
+
+        setCount((currValue)=>{
+            console.log(currValue);
+            return currValue+1;
+        })
     }
     return(
         <>
